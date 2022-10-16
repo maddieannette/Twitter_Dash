7c4897709a1b09e930cd4e01f238fc96b003aab9
maddieannette <64941576+maddieannette@users.noreply.github.com>
2022-10-16 14:29:55 -0500

    Popular Cities Looped

--- main.py
+++ main.py
@@ -22,39 +22,47 @@ api = tweepy.API(auth)
 # trends = api.get_place_trends(id = woeid)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+most_visited = pd.read_csv('wiki60visited.csv')
+most_visited_list = most_visited["City"].values.tolist()
+# print(most_visited_list)
+
+
 
 # read json to Dataframe 
 dfcodes = pd.read_json('wcodes.json')
-# test will small values
-# smalldf = dfcodes.head(4)
+# get list of most visited 
+dfcodes = dfcodes[dfcodes['name'].isin(most_visited_list)]
+print(dfcodes)
 # print(dfcodes)
-# dataframe to list to iterate through
-wcodeslist = dfcodes["woeid"].values.tolist()
-# print(wcodeslist)
 
-woeid = ''
+
+# # test will small values
+# # smalldf = dfcodes.head(4)
+# # print(dfcodes)
+# # dataframe to list to iterate through
+wcodeslist = dfcodes["woeid"].values.tolist()
+print(len(wcodeslist))
+
+
+codes_trends_list = []
+# woeid = ''
 # loop through codes list 
 for wtrends in wcodeslist:
     # print(str(wtrends) + "test")
     trends = api.get_place_trends(id = wtrends)
     for value in trends:
         for trend in value['trends']:
-            print(str(wtrends) + " " + trend['name'])
+            trend_name = trend['name']
+            woeid_codes = wtrends
+            # print(trend_name)
+            # print(woeid_codes)
+            codes_trends_list.append({'trend_name': str(trend_name),
+                                      'woeid_codes': str(woeid_codes)})
+            #trends_per_country = (str(wtrends) + " " + trend['name'])
+            
+            # print(trends_per_country)
         # print(type(trend))
         
-        # print(trends)
-# Get World City Codes
-# cityCodes = getCityCodesFromJson(jsonFilePath)
-# trendByCity = getTrendsForCityList(cityCodes)
-# saveToAwsDatabase(trendByCity)
-
-# def getCityCodesFromJson
-#   return json.load(data_file)
-
-# def getTrendsForCityList(cityCodes)
-# returnList = {}
-# for wdata in cityCodes
-#       cityTrends api.get_place_trends(id = woeid)
-#       returnList.push(wdata.citycode,cityTrends)
-
-# 
+print(type(codes_trends_list))
+# df_trends_list = pd.DataFrame(codes_trends_list, columns = ['trend', 'woeid'])
+# print(df_trends_list)
